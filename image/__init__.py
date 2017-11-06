9579c590f949aaf1b159783ae05f4f744c6dc11e
andrevaETH <vanoncini@hotmail.com>
2017-11-06 17:44:14 -0500

    Initial commit

---
+++ image/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Nov 06 2017 2:38 PM
+@author: andrevaETH
+"""
---
+++ image/base_image.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Nov 06 2017 2:38 PM
+@author: andrevaETH
+"""
+
+
+class Base_Image:
+    """
+    Baseclass for images
+    """
+
+    def __init__(self):
+        """
+
+        """
+
+    def
